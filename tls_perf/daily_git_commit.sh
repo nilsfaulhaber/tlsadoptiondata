d4dadfd6fb0a67efbda7f3075e876deeb9c3b4e3
Nils Faulhaber <ga87bub@mytum.de>
2019-08-30 11:55:45 +0200

    remove code

--- tls_perf/daily_git_commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /data/tlsadoptiondata
-git add Adoption/tlsadoption.csv
-git add Performance/
-d=$(date +%Y-%m-%d)
-git commit -m "daily commit $d"
-git push
--- tls_perf/heroku_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /data/monitoring-tls13-adoption/
-d=$(date +%Y-%m-%d)
-sudo git pull
-sudo git add tlsadoptiondata
-sudo git commit -m "daily commit on $d"
-sudo git push heroku master 
-
